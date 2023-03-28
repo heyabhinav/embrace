ddbb760d02fd497672acb0f88262b839e27f115c
heyabhinav <abhinav0050.be21@chitkara.edu.in>
2023-03-29 00:31:29 +0530

    Added Companies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Companies from './components/Companies/Companies';
 import LandingPage from './components/LandingPage/LandingPage';
 
 function App() {
   return (
     <>
       <LandingPage/>
+      <Companies/>
     </>
   );
 }
