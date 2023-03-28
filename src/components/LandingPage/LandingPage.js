c54c3dd4c2548ad5972a0ccd83a1f9693f019068
heyabhinav <abhinav0050.be21@chitkara.edu.in>
2023-03-28 23:47:45 +0530

    Created landing page

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,152 @@
+import React from "react";
+import './LandingPage.css'
+
+function LandingPage(){
+    return (
+        <>
+        <div className="header">
+          <div className="landing-page-frame97">
+            <div className="landing-page-frame96">
+              <div className="landing-page-group21841">
+                <span className="brand-logo landing-page-text165">
+                  <span>Embrace</span>
+                </span>
+                <img
+                  src="/images/Ellipse 87.jpg"
+                  alt="Ellipse871684"
+                  className="header-circle"
+                />
+                <img
+                  src="/images/Polygon 1.jpg"
+                  alt="Polygon11685"
+                  className="header-arrow"
+                />
+              </div>
+              <div className="nav-bar">
+                <span className="nav-bar-text">
+                  <span>How it Works</span>
+                </span>
+                <span className="nav-bar-text">
+                  <span>Our Work</span>
+                </span>
+                <span className="nav-bar-text">
+                  <span>Pricing</span>
+                </span>
+                <span className="nav-bar-text">
+                  <span>About Us</span>
+                </span>
+              </div>
+            </div>
+            <div className="try-button">
+              <span className="try-button-text">
+                <span>Try it Now</span>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="landing-page-hero-section">
+            <div className="landing-page-group589">
+                <span className="landing-page-text">
+                    <span>
+                        <span>We Take Care of</span>
+                        <br></br>
+                        <span>Your Brand</span>
+                    </span>
+                </span>
+                <span className="landing-page-sub-text">
+                    <span>
+                        Use this section to describe your company and the products you
+                        offer. You could share your company's story and details about
+                        why you are in business.
+                    </span>
+                </span>
+                <div className="connect">
+                    <div className="email-div">
+                        <span className="email">
+                            <span>Enter your Email</span>
+                        </span>
+                    </div>
+                    <div className="lets-talk-div">
+                        <span className="lets-talk">
+                            <span>Lets Talk</span>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="img-group landing-page-group237498">
+          {/* <img
+            src="/playground_assets/vector1694-c85.svg"
+            alt="Vector1694"
+            className="landing-page-vector5 landing-page-vector5"
+          /> */}
+          <div className="img-stack landing-page-group237494">
+            <img
+              src="images/Rectangle 196.svg"
+              alt="Female1"
+              className="img-stack-basic rectangle196"
+            />
+            <img
+              src="/images/Rectangle 200.svg"
+              alt="Rectangle2001699"
+              className="img-stack-basic rectangle200"
+            />
+            <img
+              src="images/Rectangle 201.svg"
+              alt="Rectangle2011700"
+              className="img-stack-basic rectangle201"
+            />
+            <img
+              src="/images/Rectangle 202.svg"
+              alt="Rectangle2021701"
+              className="img-stack-basic rectangle202"
+            />
+            <img
+              src="/images/Ellipse 84.svg"
+              alt="Ellipse841702"
+              className="img-stack-basic ellipse84"
+            />
+            <img
+              src="/images/Rectangle 204.svg"
+              alt="Rectangle2041697"
+              className="img-stack-basic rectangle204"
+            />
+            <img
+              src="images/Rectangle 205.svg"
+              alt="Rectangle2051698"
+              className="img-stack-basic rectangle205"
+            />
+            
+            {/* <img
+              src="images/union1706-wmw.svg"
+              alt="message cloud 1"
+              className="cloud-1 landing-page-union1"
+            />
+            <img
+              src="/images/union1703-802h.svg"
+              alt="message cloud 2"
+              className="cloud-2 landing-page-union"
+            /> */}
+            
+          </div>
+          {/* <span className="landing-page-text177">
+            <span>✏️</span>
+          </span>
+          <span className="landing-page-text179">
+            <span>👩🏻‍💻</span>
+          </span>
+          <span className="landing-page-text181">
+            <span>👑</span>
+          </span>
+          <span className="landing-page-text183">
+            <span>🤌</span>
+          </span>
+          <span className="landing-page-text185">
+            <span>😍</span>
+          </span> */}
+        </div>
+        </>
+    )
+}
+
+export default LandingPage;
