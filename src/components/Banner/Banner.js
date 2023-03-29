7512983e60e9c6e333abee3a3d0554e4bba0d42a
heyabhinav <abhinav0050.be21@chitkara.edu.in>
2023-03-29 15:11:35 +0530

    Created banner

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,66 @@
+import React from "react";
+import './Banner.css';
+
+function Banner(){
+    return(
+        <>
+        <div className="banner">
+            <div className="banner-container">
+                <div className="maskgroup">
+                    <div className="landing-page-group584">
+                        <img
+                        src="/images/Ellipse 74.svg"
+                        alt="Ellipse741558"
+                        className="ellipse74"
+                        />
+                        <img
+                        src="/images/Ellipse 77.svg"
+                        alt="Ellipse771559"
+                        className="ellipse77"
+                        />
+                        
+                        <img
+                        src="/images/Ellipse 75.svg"
+                        alt="Ellipse751560"
+                        className="ellipse75"
+                        />
+                        <img
+                        src="/images/Ellipse 76.svg"
+                        alt="Ellipse761561"
+                        className="ellipse76"
+                        />
+                        
+                        
+                    </div>
+                </div>
+                <span className="banner-heading">
+                    <span>Start your journey with us now</span>
+                </span>
+                <div className="banner-text">
+                    <span className="text">
+                        <span>
+                        Use this section to describe your company and the products you
+                        offer. You could share your company's story and details about
+                        why you are in business. Use this section to describe your
+                        company and the products you offer. You could share your
+                        company's story and
+                        </span>
+                    </span>
+                    <div className="learn-more-button">
+                        <span className="learn-more-text">
+                            <span>Learn More</span>
+                        </span>
+                        <img
+                        src="/images/Vector-white-arrow.svg"
+                        alt="Vector1567"
+                        className="vector3"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default Banner;
