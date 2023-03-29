5cf292af062652d8be2193e480ec6690c6dc0594
heyabhinav <abhinav0050.be21@chitkara.edu.in>
2023-03-29 17:06:15 +0530

    Created StartYourJourney section

---
+++ src/components/StartJourney/StartJourney.js
@@ -0,0 +1,38 @@
+import React from "react";
+import './StartJourney.css';
+
+function StartJourney(){
+    return (
+        <>
+        <div className="banner2">
+            <div className="banner2-container">
+                <div className="landing-page-frame241">
+                    <span className="start-now-text">
+                        <span>Start Now</span>
+                    </span>
+                </div>
+                <span className="start-journey-text">
+                    <span>start your journey with us now</span>
+                </span>
+                <img
+                src="/images/Ellipse 86.svg"
+                alt="Ellipse861495"
+                className="ellipse86"
+                />
+                <img
+                src="/images/Growth-blue.svg"
+                alt="Vector1496"
+                className="vector1"
+                />
+                <img
+                src="/images/Lightning-blue.svg"
+                alt="Vector1497"
+                className="vector2"
+                />
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default StartJourney;
