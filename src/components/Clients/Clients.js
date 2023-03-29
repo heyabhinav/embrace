838fa4ad7ee829c0f53838cde213437372723de3
heyabhinav <abhinav0050.be21@chitkara.edu.in>
2023-03-29 16:45:47 +0530

    Created testimonial section

---
+++ src/components/Clients/Clients.js
@@ -0,0 +1,123 @@
+import React from "react";
+import './Clients.css'
+
+function Clients(){
+    return (
+        <>
+        <div className="testimonial">
+            <div className="our-client-box">
+                <span className="our-client-heading">
+                <span>What Our Client Said about us</span>
+                </span>
+                <span className="our-client-text">
+                <span>
+                    Use this section to describe your company
+                </span>
+                </span>
+            </div>
+            <div className="landing-page-frame79">
+                <div className="review-1">
+                    <div className="review-1-box">
+                        <img
+                        src="/images/Ellipse 80.svg"
+                        alt="Ellipse801505"
+                        className="ellipse80"
+                        />
+                        <div className="review-1-name-designation">
+                            <div className="review-1-heading">
+                                <span className="review-1-name">
+                                <span>Amelia Joseph</span>
+                                </span>
+                                <span className="review-1-designation">
+                                <span>Chief Manager</span>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <span className="review-1-body">
+                        <span>
+                        Use this section to describe your company and the products you
+                        offer. You could share your company’s story and details about
+                        why you are in business.
+                        </span>
+                    </span>
+                </div>
+                <div className="review-1 review-2">
+                    <div className="review-1-box">
+                        <img
+                        src="/images/Ellipse 80-1.svg"
+                        alt="Ellipse801513"
+                        className="ellipse801"
+                        />
+                        <div className="review-1-name-designation">
+                            <div className="review-1-heading">
+                                <span className="review-1-name review-2-name">
+                                <span>Jacob Joshua</span>
+                                </span>
+                                <span className="review-1-designation review-2-designation">
+                                <span>Chief Manager</span>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <span className="review-1-body review-2-body">
+                        <span>
+                        Use this section to describe your company and the products you
+                        offer. You could share your company’s story and details about
+                        why you are in business.
+                        </span>
+                    </span>
+                </div>
+                <div className="review-1 review-2 landing-page-frame22">
+                <div className="review-1-box">
+                    <img
+                    src="/images/Ellipse 80-2.svg"
+                    alt="Ellipse801521"
+                    className="ellipse802"
+                    />
+                    <div className="review-1-name-designation">
+                        <div className="review-1-heading">
+                            <span className="review-1-name review-2-name">
+                                <span>Jacob Joshua</span>
+                            </span>
+                            <span className="review-1-designation review-2-designation">
+                                <span>Chief Manager</span>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <span className="review-1-body review-2-body">
+                    <span>
+                    Use this section to describe your company and the products you
+                    offer. You could share your company’s story and details about
+                    why you are in business.
+                    </span>
+                </span>
+                </div>
+            </div>
+          <img
+            src="/images/Ellipse 78.svg"
+            alt="Blue Circle"
+            className="bluecircle"
+          />
+          <img
+            src="/images/Ellipse 79.svg"
+            alt="White Circle"
+            className="whitecircle"
+          />
+          <img
+            src="/images/Vector-white-arrow.svg"
+            alt="Right Arrow"
+            className="right-white"
+          />
+          <img
+            src="/images/client-arrow-2.svg"
+            alt="Left Arrow"
+            className="left-grey"
+          />
+        </div>
+        </>
+    );
+}
+
+export default Clients;
